fd5e66e680160a2eb01d51b93d7f0b208e03ac92
vincent-landeau <71248064+vincent-landeau@users.noreply.github.com>
2020-12-16 17:35:56 +0100

    Modi content-header

--- acwd-child/template-parts/content-header.php
+++ acwd-child/template-parts/content-header.php
@@ -14,8 +14,8 @@
   <div class="container">
     <div class="row">
       <div class="col-auto mx-auto">
-        <p class="font-weigh-bolder h1 text-white text-center"><?php echo get_field('rubrique_header') ?></p>
-        <p class="h2 text-white text-center"><?php echo get_the_title(); ?></p>
+        <p class="font-weigh-bolder h1 text-white text-center"><?php echo get_field('titre_header') ?></p>
+        <p class="h2 text-white text-center"><?php echo get_field('sous_titre_header') ?></p>
       </div>
     </div>
   </div>
