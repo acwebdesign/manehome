c92b64c85d582fde9dcd38a488b20011525dac81
vincent-landeau <71248064+vincent-landeau@users.noreply.github.com>
2020-12-17 14:48:51 +0100

    Ajout CPT

--- acwd-child/template-realisation.php
+++ acwd-child/template-realisation.php
@@ -19,17 +19,24 @@ get_template_part( 'template-parts/content', 'header');
 	<h1><?php echo get_field('titre_realisations') ?></h1>
 	<div class="chapo"><?php echo get_field('chapo_realisations') ?></div>
 	<div class="row">
-		<?php if (have_rows('blocs_realisations')):
-			while (have_rows('blocs_realisations')): the_row(); ?>
+		<?php
+		$realisations = array(
+			'post_type' => 'realisations',
+			'post_per_page' => -1,
+			'orderby'	=> 'date'
+		);
+		$wp_realisations = new WP_Query($realisations);
+		if( $wp_realisations->have_posts() ) :
+			while( $wp_realisations->have_posts() ) : $wp_realisations->the_post();?>
 			<div class="col-6">
-				<div class="m-3 p-3" style="background: url(<?php echo get_sub_field('image_fond')['url'] ?>); background-size: cover; background-position: center;">
+				<div class="m-3 p-3" style="background: url(<?php echo get_field('image_de_fond')['url'] ?>); background-size: cover; background-position: center;">
 					<!-- <?php $img_realisation = get_sub_field('image'); ?>
 					<div class="col-5">
 						<img src="<?php echo $img_realisation['url'] ?>" alt="<?php echo $img_realisation['alt'] ?>" width="100%">
 					</div>
 					<div class="col-7"> -->
-						<span class="prenom"><?php echo get_sub_field('titre'); ?></span>
-						<p><?php echo get_sub_field('texte'); ?></p>
+						<span class="titre"><?php echo get_the_title(); ?></span>
+						<p><?php echo get_field('texte'); ?></p>
 					<!-- </div> -->
 				</div>
 			</div>
