38c276dd4fe53596a40314df74ad25f8cf7006aa
vincent-landeau <71248064+vincent-landeau@users.noreply.github.com>
2020-12-18 17:08:22 +0100

    Création templates

--- acwd-child/template-contact.php
+++ acwd-child/template-contact.php
@@ -13,55 +13,28 @@
  */
 
 get_header();
+get_template_part( 'template-parts/content', 'header');
 ?>
 
-<style type="text/css">
-
-.acf-map {
-	width: 100%;
-	height: 400px;
-	border: #ccc solid 1px;
-	margin: 20px 0;
-}
-
-/* fixes potential theme css conflict */
-.acf-map img {
-   max-width: inherit !important;
-}
-
-</style>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-        
 			<div class="container">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-			<?php the_content(); ?>
 				<div class="row">
-					<div class="col-12 col-md-6 ">
-						<?php 
-						$contact = get_field('formulaire_contact');
-						// preprint($contact);
-						echo do_shortcode('[contact-form-7 id="'.$contact->ID.'" title="'.$contact->post_title.'"]');
-						?>
-					</div>
-					<div class="col-12 col-md-6 ">
+
+					<div class="col-12">
 						<p>
 							<h5><?php bloginfo( 'name' ); ?></h5><br />
 							<?php echo get_theme_mod( 'company_address' ); ?><br />
 							<?php echo get_theme_mod( 'company_zipcode' ); ?> <?php echo get_theme_mod( 'company_city' ); ?><br />
+							<a href="mailto:<?php echo get_theme_mod('company_email'); ?>"><?php echo get_theme_mod('company_email'); ?></a><br />
 							<a href="tel:<?php echo '+33'.substr(str_replace(' ','',get_theme_mod('company_phone')),1); ?>"><?php echo get_theme_mod('company_phone'); ?></a>
 						</p>
-						<img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title();?>" class="aligncenter contact_image">
-
-						<?php
-						$map = get_field('google_map');
-						// preprint($map);
-						if( !empty($map) ):
-						?>
-						<div class="acf-map">
-							<div class="marker" data-zoom="10" data-lat="<?php echo $map['lat']; ?>" data-lng="<?php echo $map['lng']; ?>"></div>
-						</div>
-						<?php endif; ?>		
+					</div>
+					<div class="col-12 col-md-6 ">
+							<?php
+							$contact = get_field('formulaire_contact');
+							echo do_shortcode('[contact-form-7 id="5" title="Contact form 1"]');
+							?>
+					</div>
+					<div class="col-12 col-md-6 ">
 					</div>
 				</div>
 			</div>
---
+++ acwd-child/template-valeur.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: Template Valeur et Engagement
+ *
+ * This is the template that displays contact page.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ACWD
+ */
+
+get_header();
+get_template_part( 'template-parts/content', 'header');
+?>
+
+					<section class="container">
+						<h1><?php echo get_field('titre_competences') ?></h1>
+						<p class="chapo"><?php echo get_field('chapo_competences') ?></p>
+						<div class="row">
+						<?php // 1. On définit les arguments pour définir ce que l'on souhaite récupérer
+							$args = array(
+							    'post_type' => 'equipe',
+							    'posts_per_page' => -1,
+									'order' => 'ASC',
+							);
+
+							// 2. On exécute la WP Query
+							$my_query = new WP_Query( $args );
+
+							// 3. On lance la boucle !
+							if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post(); ?>
+							<div class="col-6">
+								<div class="row bg-warning m-3">
+									<?php $img_compétences = get_the_post_thumbnail_url(); ?>
+									<div class="col-5">
+										<img src="<?php echo $img_compétences; ?>" alt="<?php echo $img_compétences;?>" width="100%">
+									</div>
+									<div class="col-7">
+										<span class="nom font-weight-bold"><?php echo get_the_title(); ?></span><br>
+										<span class="competence"><?php echo get_field('role'); ?></span><br>
+										<p><?php echo get_the_content(); ?></p>
+									</div>
+								</div>
+							</div>
+						<?php
+						endwhile;
+						endif;
+
+						// 4. On réinitialise à la requête principale (important)
+						wp_reset_postdata();
+						 ?>
+							<div class="col-6">
+								<div class="row m-3 justify-content-center align-items-center">
+									<?php $img_last = get_field('dernier_bloc'); ?>
+									<img src="<?php echo $img_last['url'] ?>" alt="<?php echo $img_last['alt'] ?>">
+								</div>
+							</div>
+						</div>
+					</section>
+					<?php get_template_part( 'template-parts/chiffres-cles'); ?>
+
+
+<?php
+get_footer();
