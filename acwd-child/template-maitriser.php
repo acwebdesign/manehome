9812fe2abbe7abfe5ad56320a27475972e033076
jean magne <jeanmagne@cegetel.net>
2020-12-21 16:41:07 +0100

    ajout template part header black

--- acwd-child/template-maitriser.php
+++ acwd-child/template-maitriser.php
@@ -13,27 +13,29 @@
  */
 
 get_header();
-get_template_part( 'template-parts/content', 'header');
+get_template_part( 'template-parts/content', 'header_black');
 ?>
 					<section class="container py-5">
-						<h1><?php echo get_field('titre_maitriser') ?></h1>
-            <div class="position-relative">
+						<h1 class="pb-5"><?php echo get_field('titre_maitriser') ?></h1>
+            <div class="position-relative py-5">
               <?php $image = get_field('image') ?>
-              <div class="col-5 position-absolute" style="top:5%; right:5%;">
+              <div class="col-5 position-absolute paragraphe-type-1" style="top:15%; right:5%;">
                 <h2><?php echo get_field('titre_image') ?></h2>
                 <p><?php echo get_field('texte_image') ?></p>
               </div>
   						<img src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>" width="100%">
             </div>
+						<div class="paragraphe-type-1">
             <h2><?php echo get_field('titre_section') ?></h2>
-            <div class="row">
-              <div class="col-6">
-                <p><?php echo get_field('texte_gauche') ?></p>
-              </div>
-              <div class="col-6">
-                <p><?php echo get_field('texte_droite') ?></p>
-              </div>
-            </div>
+	            <div class="row">
+	              <div class="col-6">
+	                <p><?php echo get_field('texte_gauche') ?></p>
+	              </div>
+	              <div class="col-6">
+	                <p><?php echo get_field('texte_droite') ?></p>
+	              </div>
+	            </div>
+						</div>
 					</section>
 
 
--- acwd-child/template-parts/content-header_black.php
+++ acwd-child/template-parts/content-header_black.php
@@ -15,8 +15,8 @@
   <div class="container">
     <div class="row">
       <div class="col-auto mx-auto">
-        <p class="font-weigh-bolder h1 text-white text-center"><?php echo get_field('titre_header') ?></p>
-        <p class="h2 text-white text-center"><?php echo get_field('sous_titre_header') ?></p>
+        <p class="font-weigh-bolder h1 text-black text-center"><?php echo get_field('titre_header') ?></p>
+        <p class="h2 text-black text-center"><?php echo get_field('sous_titre_header') ?></p>
       </div>
     </div>
   </div>
