8a2f0e6ef11aa2298191c29dc78cc38ebbe95076
oceanehang <71276227+oceanehang@users.noreply.github.com>
2020-12-22 15:20:59 +0100

    presse : intégration

--- acwd-child/template-presse.php
+++ acwd-child/template-presse.php
@@ -15,9 +15,15 @@
 get_header();
 get_template_part( 'template-parts/content', 'header');
 ?>
-
+<style>
+	.texte-face{
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%); /* décalage de 50% de sa propre taille */
+	}
+</style>
 					<section class="container">
-						<h1><?php echo get_field('titre_competences') ?></h1>
+						<!-- <h1><?php echo get_field('titre_competences') ?></h1>
 						<div class="row">
 							<?php if (have_rows('blocs_faq')):
 								while (have_rows('blocs_faq')): the_row(); ?>
@@ -30,6 +36,43 @@ get_template_part( 'template-parts/content', 'header');
 										<?php echo get_sub_field('reponse') ?>
 									</div>
 								</div>
+							<?php endwhile; endif; ?>
+						</div> -->
+						<div class="row my-5 text-center ">
+							<div class="col">
+								<h1 class="mb-3"><?php echo get_field('titre_introduction') ?></h1>
+								<p class="chapo">
+									<?php echo get_field('texte_introduction', false,false) ?>
+								</p>
+							</div>
+						</div>
+						<div class="row">
+							<?php if (have_rows('bloc_presse')):
+								while (have_rows('bloc_presse')): the_row(); ?>
+								<div class="col-6">
+									<div class="flip my-3">
+										<div class="card">
+											<div class="face front">
+												<div class="inner text-center">
+													<div class="texte-face position-absolute">
+														<img src="https://img.icons8.com/metro/80/000000/download.png" class="pb-3"/>
+														<h2 class="text-uppercase"><?php echo get_sub_field('titre') ?></h2>
+													</div>
+
+													<?php $bg_img = get_sub_field('image_de_fond')?>
+													<img src="<?php echo $bg_img['url'] ?>" alt="<?php echo $bg_img['alt'] ?>" width="100%">
+												</div>
+											</div>
+											<div class="face back">
+												<div class="inner row justify-content-center align-items-center">
+													<p class="position-absolute text-center"><?php echo get_sub_field('texte_au_survol') ?></p>
+													<?php $bg_img_hover = get_sub_field('image_de_fond_au_survol')?>
+													<img src="<?php echo $bg_img_hover['url'] ?>" alt="<?php echo $bg_img_hover['alt'] ?>" width="100%">
+												</div>
+										</div>
+									</div>
+								</div>
+							</div>
 							<?php endwhile; endif; ?>
 						</div>
 					</section>
